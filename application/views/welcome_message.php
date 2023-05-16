fa3b28bb095ca458713cfb516201838fb8aa98dc
ERICK VLADIMIR VACA QUEVEDO <erick.vaca1605@utc.edu.ec>
2023-05-16 11:40:20 -0500

    segundo commit

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,4 +2,5 @@
 <img src="<?php echo base_url(); ?>/assets/images/img1.png" alt="">
 
 	<h1 style="color:white; background-color:#39B3F5";> bienvenido al restaurante populer <br>Tenemos bariedades de comida para cualquier tipo de gustos </h1>
+	<h1>ella no te ama</h1>
 </center>
